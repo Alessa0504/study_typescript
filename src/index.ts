dcfc3ef04ec64ea46ecfa9ba263442f018494267
Alessa0504 <vivianjjz@163.com>
2023-03-26 22:29:53 +0800

    feat: 配置Babel，保证兼容性

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ import {hi} from './m1';   //引入hi
 
 console.log("哈哈");
 console.log(hi);
+
+//测试浏览器低版本编译后的bundle.js(ie 11没有const)
+const person = {name:"jillian", age:30}
+person.age = 18
+console.log(person);
+console.log(Promise);  //ie不支持Promise
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,11 @@ module.exports = {
         //打包后的目录
         path: path.resolve(__dirname, 'dist'),  //利用path库拼接路径，等价于'./dist'
         //打包后的文件名
-        filename: "bundle.js"
+        filename: "bundle.js",
+        //配置打包环境
+        environment: {
+            arrowFunction: false  //告诉webpack不使用()=>函数，为了兼容ie
+        }
     },
 
     //指定webpack打包时要用到的模块
@@ -28,7 +32,35 @@ module.exports = {
                 //test指定规则生效的文件
                 test: /\.ts$/,   // '/ /'为正则，'\'为转义，用正则表达式匹配以.ts为结尾的文件
                 //use表示用什么去处理test指定的文件
-                use: 'ts-loader',  //用ts-loader处理.ts文件
+                use: [   //要用到多个加载器所以使用数组，从下往上执行loader(即先1后2)
+                    // 配置babel--把新版js转为旧版js-2
+                    {
+                        //指定加载器
+                        loader: "babel-loader",
+                        options: {
+                            //设置babel兼容的浏览器(预定义环境)
+                            presets: [
+                                [
+                                    //指定环境插件
+                                    "@babel/preset-env",
+                                    //配置信息
+                                    {
+                                        //要兼容的浏览器版本
+                                        targets: {
+                                            "chrome":"58",
+                                            "ie":"11"
+                                        },
+                                        //指定corejs版本
+                                        "corejs":"3",
+                                        //使用corejs的方式, "usage"表示按需加载
+                                        "useBuiltIns":"usage"
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    'ts-loader'  //用ts-loader处理.ts文件。把ts转为js-1
+                ],
                 //exclude 要排除的文件
                 exclude: /node_modules/   //排除node_modules
             }
